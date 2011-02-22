bb4b4b19e869d4e7ed88927921a0cc6d83cf5406
Mikeal Rogers <mikeal.rogers@gmail.com>
2011-02-21 22:42:12 -0800

    Initial commit.

---
+++ bin.js
@@ -0,0 +1,18 @@
+var rfinder = require('./main');
+
+var argv = process.argv.slice(2)
+  , port = 80
+  , folders = []
+  ;
+
+argv.forEach(function (a) {
+  if (!isNaN(parseInt(a))) {
+    port = parseInt(a);
+  } else {
+    folders.push(a);
+  }
+})
+
+rfinder.createServer(folders, function (server) {
+  server.listen(port, function () {console.log("Serving on "+port+"...")});
+})
---
+++ main.js
@@ -0,0 +1,71 @@
+var fs = require('fs')
+  , watch = require('watch')
+  , http = require('http')
+  , paperboy = require('paperboy')
+  , path = require('path')
+  , spawn = require('child_process').spawn
+  ;
+  
+var folders = ['/Users/mikeal/Movies']
+  , WEBROOT = path.join(__dirname, 'static')
+  , monitors = []
+  ;
+
+exports.createServer = function (folders, cb) {
+  folders.forEach(function (root) {
+    watch.createMonitor(root, function (monitor) {
+      monitors.push(monitor);
+      if (monitors.length === folders.length) {
+        var s = http.createServer(function (req, res) {
+          if (req.method === 'POST') {
+            var buf = '';
+            req.on('data', function (c) {
+              buf += c;
+            })
+            req.on('end', function () {
+              var body = JSON.parse(buf);
+              
+              if (req.url === '/open') {
+                spawn("/usr/bin/open", [body.filename]);
+              } else {
+                var foundfiles = [];
+                monitors.forEach(function (monitor) {
+                  for (f in monitor.files) {
+                    if (f.slice(f.lastIndexOf('/')).indexOf(body.search) !== -1) {
+                      foundfiles.push(f);
+                    }
+                  }
+                })
+                res.writeHead('200', {'content-type':'application/json'});
+                res.write(JSON.stringify(foundfiles));
+                res.end();
+              }
+            })
+          } else {
+            var ip = req.connection.remoteAddress;
+            paperboy
+              .deliver(WEBROOT, req, res)
+              .addHeader('Expires', 300)
+              .addHeader('X-PaperRoute', 'Node')
+              .error(function(statCode, msg) {
+                res.writeHead(statCode, {'Content-Type': 'text/plain'});
+                res.end("Error " + statCode);
+              })
+              .otherwise(function(err) {
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.end("Error 404: File not found");
+              });
+          }
+        })
+        cb(s);
+      }
+    })
+  })
+  
+}
+
+
+
+
+
+
